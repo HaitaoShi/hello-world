4a9dce8a3330eabdfd5716729850cd67bf4ef2ef
shihaitao <451483204@qq.com>
2020-05-03 08:18:52 +0100

    how to connect

--- learning.R
+++ learning.R
@@ -1,4 +1,12 @@
-I am starting to learn version control today.
-second try.
-6*9
-
+Key points
+•	In terminal: configure git 
+git config --global user.name "Your Name"
+git config --global user.email "your@email.com"
+•	In RStudio, create project > Version control > Git
+•	Git pane: status symbols and color
+•	Git actions:
+1.	pull: pull changes from remote repo (if you are in collaboration with others)
+2.	add: stage files for commit; click on stage box under git pane
+3.	commit: commit to the local repo; click on "commit" button under git pane; add a commit message
+4.	push: push to the remote repo on Github
+•	.gitignore file: details check https://git-scm.com/docs/gitignore
